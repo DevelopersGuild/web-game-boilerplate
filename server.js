02996e2e5aa54131661e7751e403f4e6adcaf819
Christopher Banh <chris@chrisbanh.com>
2016-04-21 14:29:29 -0700

    Add comments to server.js

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+// Load Express into our app
 var express = require('express');
+
+// Load Nunjucks into our app
 var nunjucks = require('nunjucks');
 
+// Create an instance of our Express app
 var app = express();
 
+// Make Nunjucks look for our HTML files inside the views/ directory
+// and make it load itself into Express
 nunjucks.configure('views', {
-    autoescape: true,
     express: app
 });
 
+// Tell Express to serve files in the public/ directory
 app.use(express.static('public'));
 
+// Make Express render index.html when someone visits on '/'
 app.get('/', function(req, res) {
 	res.render('index.html');
 })
 
+// Have our Express server listen on port 3040
 app.listen(3040);
 
+// Print this message to console
 console.log('Server is now running at localhost:3040');
